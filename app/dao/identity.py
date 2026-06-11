7a4039998462ee7af4915f532ae248f4f406fd76
Addd007 <1346498750@qq.com>
2026-06-11 18:02:57 +0800

    1

--- app/dao/identity.py
+++ app/dao/identity.py
@@ -7,7 +7,6 @@ from flask import current_app
 
 from app.core.db import db
 from app.libs.enums import AtLeastEnum, ClientTypeEnum
-from app.models.user import User
 from app.models.identity import Identity
 from app.libs.error_code import AtLeastOneClientException
 
@@ -18,10 +17,13 @@ class IdentityDao():
     # 绑定用户
     @staticmethod
     def bind(user_id, identifier, type):
+        credential = None
+        if ClientTypeEnum(type) in current_app.config['CLINET_INNER_TYPES']:
+            credential = IdentityDao.get_credential(user_id=user_id)
         IdentityDao.create_identity(
             user_id=user_id,
             identifier=identifier,
-            credential=None,
+            credential=credential,
             type=type)
 
     # 解绑用户
@@ -37,18 +39,9 @@ class IdentityDao():
     # 新建身份
     @staticmethod
     def create_identity(user_id, identifier, credential, type):
-        # 判断是否是网站内部的身份类型
-        if ClientTypeEnum(type) in current_app.config['CLINET_INNER_TYPES']:
-            with db.auto_commit():
-                user = User.get(id=user_id)
-                attr = ClientTypeEnum(type).name.lower()
-                setattr(user, attr, identifier)
-                Identity.create(commit=False, user_id=user_id, type=type,
-                                identifier=identifier, credential=credential)
-                user.save(commit=False)
-        # 第三方平台，则无需修改用户信息
-        else:
-            Identity.create(user_id=user_id, type=type, identifier=identifier)
+        with db.auto_commit():
+            Identity.create(commit=False, user_id=user_id, type=type,
+                            identifier=identifier, credential=credential)
 
     # 更新身份
     @staticmethod
@@ -64,23 +57,14 @@ class IdentityDao():
     # 删除身份
     @staticmethod
     def delete_identity(user_id, type):
-        '''删除时，需要判断type的类型
-           因为username、mobile、email的解绑需要清除user表的对应字段
-        '''
-        user = User.get(id=user_id)
         with db.auto_commit():
-            # 判断是否是网站内部的身份类型
-            if ClientTypeEnum(type) in current_app.config['CLINET_INNER_TYPES']:
-                attr = ClientTypeEnum(type).name.lower()
-                setattr(user, attr, None)
-                user.save(commit=False)
             identity = Identity.get_or_404(user_id=user_id, type=type)
             identity.hard_delete(commit=False)  # 硬删除
 
     # 获取加密后的密码
     @staticmethod
     def get_credential(user_id):
-        credential, = db.session.query(Identity._credential).filter(
+        result = db.session.query(Identity._credential).filter(
             Identity.user_id == user_id,
             Identity.type.in_([
                 ClientTypeEnum.USERNAME.value,
@@ -88,4 +72,4 @@ class IdentityDao():
                 ClientTypeEnum.MOBILE.value]),
             Identity._credential != None
         ).first()
-        return credential
+        return result[0] if result else None
